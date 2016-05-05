4d5d1e782da47a6ca05525d25f27540ca23f58c1
Azzyxec <azzyxec@gmail.com>
2016-05-05 14:42:40 +0530

    configuration file

---
+++ AppConfig.php
@@ -0,0 +1,16 @@
+<?php
+
+class AppConfig{
+
+  //slim config
+  public static $templatePath = '../pms/templates';
+  public static $displayErrorDetails = true;
+
+  //Database config
+  public static $dbhost = 'localhost';
+  public static $dbname = 'pms';
+  public static $port = '3306';
+  public static $dbuser = 'pms';
+  public static $dbpass = 'pms';
+
+}
