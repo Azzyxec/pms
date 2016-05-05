e2ec22886fc24baba2806fbd48366318d0b4ecbb
Azzyxec <azzyxec@gmail.com>
2016-05-05 13:15:19 +0530

    config file

---
+++ config.php
@@ -0,0 +1,9 @@
+class AppConfig{
+
+  public static $dbhost = 'localhost';
+  public static $dbname = 'pms';
+  public static $port = '3306';
+  public static $dbuser = 'pms';
+  public static $dbpass = 'pms';
+
+}
