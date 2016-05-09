8763b6b9d46e4de6a73fd3360e7081ec49b4b3fd
nicolasXec <akhilshaikh28@gmail.com>
2016-05-09 12:10:17 +0530

    updated functionality for book appointmetn

--- devScripts/appointment/new.appointment.js
+++ devScripts/appointment/new.appointment.js
@@ -4,4 +4,40 @@ $(document).ready(function(){
         console.log('new appointment js loaded');
     }());
 
+
+    
+     function getBookAppointmentFormDetails(){
+                var name = $("#book-appointment-patientsName-form-input").val();
+                var date = $("#book-appointment-date-form-input").val();
+                var time =  $("#book-appointment-patientsTime-form-input").val();
+                var contact = $("#book-appointment-contact-form-input").val();
+                var description = $("#book-appointment-description-form-input").val();
+             var bookAppointmentFormData = {patientsName:name,bookAppointmentDate:date,bookAppointmentTime:time,contact:contact,description:description};
+         return bookAppointmentFormData;
+        };
+    $("#book-appointment-section-form-submit-btn").click(function(){
+     
+        console.log("yay book appointment submition started");
+      
+        
+      
+            var bookAppointmentFormJsonData = JSON.stringify(getBookAppointmentFormDetails());
+           
+           
+        
+            $.ajax({
+                url:"index.php/saveBookPatientEntry",
+                type:"post",
+                dataType:"json",
+                data:{data:bookAppointmentFormJsonData},
+                cache:false,
+                success:function(response){
+                    console.log(response);
+                },
+                
+                
+            });
+        
+    });
+
 });
