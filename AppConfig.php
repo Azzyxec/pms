41ee7d34a9d9ec6e91533dae156e43a2f0c2325f
nicolasXec <akhilshaikh28@gmail.com>
2016-05-22 18:13:53 +0530

    updated Appconfig to incoparete my system settings

--- AppConfig.php
+++ AppConfig.php
@@ -12,3 +12,22 @@ class AppConfig{
   public static $dbuser = 'pms';
   public static $dbpass = 'pms';
 }
+
+
+/* NicolasXec Configurations
+
+class AppConfig{
+  //slim config
+  public static $templatePath = '../app/templates';
+  public static $displayErrorDetails = true;
+  public static $basePath = '/public/';
+  //Database config
+  public static $dbhost = 'localhost';
+  public static $dbname = 'pms';
+  public static $port = '3306';
+  public static $dbuser = 'root';
+  public static $dbpass = '';
+}
+
+*/
+?>
