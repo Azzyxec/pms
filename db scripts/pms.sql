9fc45c08e0d4411f84e6568f9b0160f6b577ece5
Azzyxec <azzyxec@gmail.com>
2016-05-23 22:35:29 +0530

    db scripts added

--- db scripts/pms.sql
+++ db scripts/pms.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 16, 2016 at 10:44 AM
+-- Generation Time: May 23, 2016 at 07:03 PM
 -- Server version: 10.1.10-MariaDB
 -- PHP Version: 7.0.4
 
@@ -320,6 +320,79 @@ select @lprogrammeListCount as status;
 
 end$$
 
+CREATE DEFINER=`root`@`localhost` PROCEDURE `add_update_patient_birth_details` (IN `ppatient_id` INT, IN `pdelivery_method_id` INT, IN `pbirth_weight` VARCHAR(20), IN `plength` VARCHAR(20), IN `phead` VARCHAR(20), IN `pblood_group` VARCHAR(10), IN `pmothers_name` VARCHAR(100), IN `pmothers_blood_group` VARCHAR(10), IN `pfathers_name` VARCHAR(100), IN `pfathers_blood_group` VARCHAR(10), IN `psiblings` VARCHAR(100), IN `puser_id` INT, IN `puser_type` VARCHAR(5), IN `pis_active` INT, IN `premarks` VARCHAR(4000))  MODIFIES SQL DATA
+begin
+
+declare lbirthEntryExists int;
+
+
+select count(*)
+into   @lbirthEntryExists
+from   patient_birth_details
+where  fk_patient_id = ppatient_id;
+
+if @lbirthEntryExists = 0 then
+
+INSERT INTO `patient_birth_details`(
+									`fk_patient_id`
+									 , `fk_delivery_method_id`
+									 , `birth_weight`
+									 , `length`
+									 , `head`
+									 , `blood_group`
+									 , `mother_name`
+									 , `mother_blood_group`
+									 , `father_name`
+									 , `father_blood_group`
+									 , `siblings`
+									 , `remarks`
+									 , `created_date`
+									 , `fk_created_by_id`
+									 , `created_by_type`
+									 , `is_active`
+									 ) 
+							VALUES (
+									ppatient_id
+									,pdelivery_method_id
+									,pbirth_weight
+									,plength
+									,phead
+									,pblood_group
+									,pmothers_name
+									,pmothers_blood_group
+									,pfathers_name
+									,pfathers_blood_group
+									,psiblings
+									,premarks
+									,now()
+									,puser_id
+									,puser_type
+									,pis_active
+									);
+else
+
+UPDATE `patient_birth_details` SET 
+									`fk_delivery_method_id` = pdelivery_method_id
+									,`birth_weight` = pbirth_weight
+									,`length`= plength
+									,`head` = phead
+									,`blood_group`= pblood_group
+									,`mother_name`= pmothers_name
+									,`mother_blood_group` = pmothers_blood_group
+									,`father_name`= pfathers_name
+									,`father_blood_group`= pfathers_blood_group
+									,`siblings` = psiblings
+									,`remarks` = premarks
+									,`modified_date` = now()
+									,`fk_modified_by_id` = puser_id
+									,`modified_by_type` = puser_type
+									,`is_active`= pis_active 
+						WHERE fk_patient_id = ppatient_id;
+end if;
+
+
+end$$
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `authenticate` (IN `plogin_id` VARCHAR(90), IN `ppassword` VARCHAR(90))  READS SQL DATA
 begin
 
@@ -327,6 +400,9 @@ declare lLoginId int;
 declare lUserType varchar(5);
 declare lname varchar(100);
 declare luserId int;
+declare ldoctorId int;
+
+set @ldoctorId = -1;
 
 select id
 	   ,`type`
@@ -341,14 +417,16 @@ if @lUserType is null then
 
     select '-1' as id
     	   ,'-1' as `type`
-           ,'-1' as name;
+           ,'-1' as name
+		   ,@ldoctorId as doctor_id;
               
               
 elseif @lUserType = 'A' then
 
 	select 1 as id
     	   ,@lUserType as `type`
-           ,'admin' as name;
+           ,'admin' as name
+		   ,@ldoctorId as doctor_id;
            
 elseif @lUserType = 'D' then
 
@@ -361,7 +439,24 @@ elseif @lUserType = 'D' then
 
 	select @luserId as id
     	   ,@lUserType as `type`
-           ,@lname as name;
+           ,@lname as name
+		   ,@luserId as doctor_id;
+		   
+elseif @lUserType = 'S' then
+
+	select first_name
+    	   ,id
+		   ,fk_doctor_id
+    into @lname
+    	 ,@luserId
+		 ,ldoctorId
+    from staff
+    where fk_user_id = @lLoginId;
+
+	select @luserId as id
+    	   ,@lUserType as `type`
+           ,@lname as name
+		   ,@ldoctorId as doctor_id;
 
 end if;
 
@@ -843,6 +938,133 @@ begin
 
 end$$
 
+CREATE DEFINER=`root`@`localhost` PROCEDURE `create_modify_staff` (IN `pid` INT, IN `pfk_doctor_id` INT, IN `pfk_location_id` INT, IN `pfirst_name` VARCHAR(100), IN `plast_name` VARCHAR(100), IN `pcontact1` VARCHAR(50), IN `pcontact2` VARCHAR(50), IN `pemail` VARCHAR(100), IN `paddress` VARCHAR(1000), IN `puser_name` VARCHAR(100), IN `ppassword` VARCHAR(100), IN `precovery_contact` VARCHAR(50), IN `precovery_email` VARCHAR(100), IN `pfk_logged_in_user_id` INT, IN `plogged_in_user_type` VARCHAR(2), IN `pis_active` INT)  MODIFIES SQL DATA
+begin
+
+	declare llogin_id_exists int;
+	declare llogin_id int;
+	declare lstaff_id int;
+
+	if COALESCE(pid, 0) > 0 then 
+
+		select fk_user_id
+		into @llogin_id
+		from staff
+		where id = pid;
+		  
+	end if;
+	
+    select count(id) 
+    into  @llogin_id_exists
+    from login
+    where login_id = puser_name
+          and id <> COALESCE(@llogin_id, 0);
+		  
+	if  COALESCE(@llogin_id_exists, 0) = 0 then
+	
+		if pid <= 0 then
+		
+			INSERT INTO `login`( `type`
+						, `login_id`
+						, `password`
+						, `created`
+						,last_modified
+						,is_active
+						)
+						VALUES 
+						('S'
+						,puser_name
+						,ppassword
+						,now()
+						,null
+						,pis_active
+						);
+								
+			 select max(id)
+			 into @llogin_id
+			 from login;
+		
+			INSERT INTO `staff`(
+								`first_name`,
+								`last_name`, 
+								`contact1`,
+								`contact2`,
+								`email`,
+								`address`,
+								`recovery_contact`,
+								`recovery_email`,
+								`fk_location_id`,
+								`fk_doctor_id`,
+								`fk_user_id`,
+								`created_date`,
+								`is_active`) 
+					   VALUES (pfirst_name,
+							   plast_name,
+							   pcontact1,
+							   pcontact2,
+							   pemail,
+							   paddress,
+							   precovery_contact,
+							   precovery_email,
+							   pfk_location_id,
+							   pfk_doctor_id,
+							   @llogin_id,
+							   now(),
+							   pis_active
+							   );
+							   
+			 select max(id)
+			 into @lstaff_id
+			 from staff;
+			 
+		else
+		
+			select fk_user_id
+			into @llogin_id
+			from staff 
+			where id = pid;
+			
+			set @lstaff_id = pid;
+			
+			UPDATE `login` 
+					SET `login_id`= puser_name
+					,`password`= ppassword
+					,`last_modified`= now()
+                    ,is_active = pis_active
+			WHERE id = @llogin_id;
+			
+			UPDATE `staff` SET  `first_name`= pfirst_name
+								,`last_name`= plast_name
+								,`contact1`= pcontact1
+								,`contact2`= pcontact2
+								,`email`= pemail
+								,`address`= paddress
+								,`recovery_contact`= precovery_contact
+								,`recovery_email`= precovery_email
+								,`fk_location_id`= pfk_location_id
+								,`fk_doctor_id`= pfk_doctor_id
+								,`is_active`= pis_active 
+							WHERE id = pid;
+				
+		end if;
+		
+		commit;
+		select '1' as status
+	   ,@lstaff_id as id
+	   ,pfirst_name as name
+	   ,'S' as `type`;
+	
+	else
+
+		select '-1' as status
+				,'-1' as id
+				,'-1' as name
+				,'-1' as `type`;
+
+	end if;
+
+end$$
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `getDoctorInfo` (IN `pid` INT)  READS SQL DATA
 SELECT
    d.name ,
@@ -885,6 +1107,12 @@ where fk_doctor_id = fk_doctor_id;
 
 
 end$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `get_delivery_methods` ()  READS SQL DATA
+select id
+	   ,name
+from delivery_methods
+where is_active = 1$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `get_medication_programme` (IN `pdoctor_id` INT, IN `pprogramme_id` INT)  READS SQL DATA
 select id
@@ -983,6 +1211,63 @@ where s.fk_doctor_id = pdoctor_id;
 
 end$$
 
+CREATE DEFINER=`root`@`localhost` PROCEDURE `get_staff_details` (IN `pid` INT)  READS SQL DATA
+begin
+
+declare luser_id int;
+declare luser_name varchar(100);
+declare lpassword varchar(100);
+
+select fk_user_id
+into   @luser_id
+from staff
+where  id = pid;
+
+SELECT `login_id`
+		,`password`
+into   @luser_name
+	   ,@lpassword
+FROM `login` 
+WHERE  id = @luser_id;
+
+SELECT `first_name`
+		, `last_name`
+		, `contact1`
+		, `contact2`
+		, `email`
+		, `address`
+		, `recovery_contact`
+		, `recovery_email`
+		, `fk_location_id`
+		, `fk_doctor_id`
+		, `fk_user_id`
+		, `created_date`
+		, `is_active`
+		,@luser_name as user_name
+		,@lpassword as password
+FROM `staff` 
+WHERE id = pid;
+
+end$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `get_staff_list_for_doctor` (IN `pdoctor_id` INT)  NO SQL
+begin
+
+SELECT   s.id
+		,s.first_name
+		, s.contact1
+		, s.email
+		, s.address
+		, date_format(s.created_date, '%d-%m-%Y') as created_date
+		, s.is_active
+		,(select name
+		  from work_locations l
+		  where l.id = s.fk_location_id) as location
+FROM staff s
+WHERE fk_doctor_id = pdoctor_id;
+
+end$$
+
 DELIMITER ;
 
 -- --------------------------------------------------------
@@ -1002,6 +1287,29 @@ CREATE TABLE `appointment` (
   `created_date` datetime NOT NULL,
   `is_active` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `delivery_methods`
+--
+
+CREATE TABLE `delivery_methods` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `is_active` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `delivery_methods`
+--
+
+INSERT INTO `delivery_methods` (`id`, `name`, `is_active`) VALUES
+(1, 'Normal', 1),
+(2, 'Vaccum', 1),
+(3, 'Forceps', 1),
+(4, 'Caesarean', 1),
+(5, 'Others', 1);
 
 -- --------------------------------------------------------
 
@@ -1045,7 +1353,7 @@ INSERT INTO `doctor` (`id`, `fk_login_id`, `name`, `contact1`, `contact2`, `emai
 (15, 47, 'Ginna Grannis', '3214234', '4234123', 'asdfad@gmail.com', 'MBBS Neurology', 'Cali', '', '', 1),
 (16, 48, 'Ginna Grannis', '3214234', '4234123', 'asdfad@gmail.com', 'MBBS Neurology', 'Cali', '', '', 1),
 (17, 49, 'Ginna Grannis', '3214234', '4234123', 'asdfad@gmail.com', 'MBBS Neurology', 'Cali', '', '', 1),
-(18, 50, 'Greg', '2341231111', '12341111', 'asdfasdf', '123411', '1234111', '3333', 'recova', 1),
+(18, 50, 'Greg', '2341231111', '12341111', 'asdfasdf', '123411', '1234111', '3333', 'recova222', 1),
 (19, 51, 'Dino', '341234', '234123', 'fdafd@gmail.com', 'asdf', 'asdf', '3241234', 'asdfsd', 1),
 (20, 52, 'Dino', '341234', '234123', 'fdafd@gmail.com', 'asdf', 'asdf', '3241234', 'asdfsd', 1),
 (21, 53, 'ddd', '2134', '2134', 'dsaf', 'asdf', 'asdf', 'asdf', 'asdf', 1),
@@ -1092,13 +1400,15 @@ INSERT INTO `login` (`id`, `type`, `login_id`, `password`, `created`, `last_modi
 (47, 'D', 'ginna12', 'ginna', '2016-05-02 11:22:50', NULL, 1),
 (48, 'D', 'ginna13', 'ginna', '2016-05-02 11:23:31', NULL, 1),
 (49, 'D', 'ginna14', 'ginna', '2016-05-02 11:23:47', NULL, 1),
-(50, 'D', 'greg', 'greg', '2016-05-02 13:44:21', '2016-05-14 01:25:54', 1),
+(50, 'D', 'greg', 'greg', '2016-05-02 13:44:21', '2016-05-19 00:00:42', 1),
 (51, 'D', 'dino', 'dino', '2016-05-04 00:02:56', '2016-05-14 01:38:07', 1),
 (52, 'D', 'dino1', 'dino1', '2016-05-04 00:03:22', NULL, 1),
 (53, 'D', 'kkkk', 'kkkk', '2016-05-04 00:04:30', NULL, 1),
 (54, 'D', 'frank', 'frank', '2016-05-04 00:16:08', NULL, 1),
 (55, 'D', 'frank2', 'frank2', '2016-05-04 00:17:25', '2016-05-04 00:33:15', 1),
-(56, 'D', 'saviopereira88', 'cipla@123', '2016-05-13 21:46:23', '2016-05-14 01:31:31', 1);
+(56, 'D', 'saviopereira88', 'cipla@123', '2016-05-13 21:46:23', '2016-05-14 01:31:31', 1),
+(58, 'S', 'mit', 'mit', '2016-05-20 00:42:29', '2016-05-20 00:43:50', 1),
+(59, 'S', 'der', 'der', '2016-05-20 21:01:12', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -1151,9 +1461,9 @@ CREATE TABLE `medication_programme_list` (
 --
 
 INSERT INTO `medication_programme_list` (`id`, `fk_medication_programme_id`, `duration_days`, `duration_text`, `medicine`, `dose_no`, `created_date`, `is_active`, `fk_doctor_id`, `modified_date`, `update_marker`) VALUES
-(1, 1, 0, '', 'BCG', 0, '2016-05-07', 1, 18, '0000-00-00 00:00:00', 0),
-(2, 1, 0, '', 'OPV', 0, '2016-05-07', 1, 18, '0000-00-00 00:00:00', 0),
-(3, 1, 0, '', 'Hepatatis B', 1, '2016-05-07', 1, 18, '0000-00-00 00:00:00', 0),
+(1, 1, 0, '', 'BCG', 0, '2016-05-07', 1, 18, '2016-05-18 23:39:27', 0),
+(2, 1, 0, '', 'OPV', 0, '2016-05-07', 1, 18, '2016-05-18 23:39:27', 0),
+(3, 1, 0, '', 'Hepatatis B', 1, '2016-05-07', 1, 18, '2016-05-18 23:39:27', 0),
 (4, 2, 14, '', 'Pnemococcal Conjugate vaccine', 1, '2016-05-09', 1, 18, '0000-00-00 00:00:00', 0),
 (5, 2, 14, '', 'DTaP-IPV/Hib', 1, '2016-05-09', 1, 18, '0000-00-00 00:00:00', 0),
 (6, 2, 14, '', 'Rotavirus', 1, '2016-05-09', 1, 18, '0000-00-00 00:00:00', 0),
@@ -1206,7 +1516,9 @@ INSERT INTO `patient` (`id`, `fk_doctor_id`, `name`, `date_of_birth`, `blood_gro
 (45, 18, 'Travolda', '2016-04-01', 'AB+', '2 kgs', '20 cms', 1, '14242341', '12412341', NULL, 'Kanas', '2.jpg', 0, NULL, '2016-05-10', 1),
 (46, 18, 'Travolda', '2016-04-01', 'AB+', '2 kgs', '20 cms', 1, '14242341', '12412341', NULL, 'Kanas', '2.jpg', 0, NULL, '2016-05-10', 1),
 (47, 18, 'Travolda', '2016-04-01', 'AB+', '2 kgs', '20 cms', 1, '14242341', '12412341', NULL, 'Kanas', '2.jpg', 0, NULL, '2016-05-10', 1),
-(48, 18, 'Travolda', '2016-04-01', 'AB+', '2 kgs', '20 cms', 1, '14242341', '12412341', NULL, 'Kanas', '2.jpg', 0, NULL, '2016-05-12', 1);
+(48, 18, 'Travolda', '2016-04-01', 'AB+', '2 kgs', '20 cms', 1, '14242341', '12412341', NULL, 'Kanas', '2.jpg', 0, NULL, '2016-05-12', 1),
+(49, 18, 'Travolda', '2016-04-01', 'AB+', '2 kgs', '20 cms', 1, '14242341', '12412341', NULL, 'Kanas', '2.jpg', 0, NULL, '2016-05-18', 1),
+(50, 18, 'Travolda', '2016-04-01', 'AB+', '2 kgs', '20 cms', 1, '14242341', '12412341', NULL, 'Kanas', '2.jpg', 0, NULL, '2016-05-18', 1);
 
 -- --------------------------------------------------------
 
@@ -1215,7 +1527,6 @@ INSERT INTO `patient` (`id`, `fk_doctor_id`, `name`, `date_of_birth`, `blood_gro
 --
 
 CREATE TABLE `patient_birth_details` (
-  `id` int(11) NOT NULL,
   `fk_patient_id` int(11) NOT NULL,
   `fk_delivery_method_id` int(11) NOT NULL,
   `birth_weight` varchar(20) NOT NULL,
@@ -1254,7 +1565,8 @@ CREATE TABLE `patient_medication_programme` (
 
 INSERT INTO `patient_medication_programme` (`id`, `fk_patient_id`, `fk_doctor_id`, `fk_medication_pogramme_id`, `name`, `created_date`, `is_active`) VALUES
 (6, 45, 18, 1, 'Newnatal', '2016-05-10', 1),
-(7, 45, 18, 2, 'Newnatal1', '2016-05-10', 1);
+(7, 45, 18, 2, 'Newnatal1', '2016-05-10', 1),
+(8, 50, 18, 6, 'Yo self b4 others', '2016-05-18', 1);
 
 -- --------------------------------------------------------
 
@@ -1289,7 +1601,10 @@ INSERT INTO `patient_medication_programme_list` (`id`, `fk_patient_id`, `fk_doct
 (45, 45, 18, 2, 6, 14, 'Rotavirus', 1, '2016-05-18', '2016-05-20', 'sssss'),
 (46, 48, 18, 1, 1, 0, 'BCG', 0, '2016-05-11', '2016-05-11', 'qwer'),
 (47, 48, 18, 1, 2, 0, 'OPV', 0, '2016-05-25', '2016-05-24', 'qwer'),
-(48, 48, 18, 1, 3, 0, 'Hepatatis B', 1, '2016-05-18', '2016-05-26', 'qwer');
+(48, 48, 18, 1, 3, 0, 'Hepatatis B', 1, '2016-05-18', '2016-05-26', 'qwer'),
+(49, 50, 18, 1, 1, 0, 'BCG', 0, '0000-00-00', '0000-00-00', ''),
+(50, 50, 18, 1, 2, 0, 'OPV', 0, '0000-00-00', '0000-00-00', ''),
+(51, 50, 18, 1, 3, 0, 'Hepatatis B', 1, '0000-00-00', '0000-00-00', '');
 
 -- --------------------------------------------------------
 
@@ -1429,6 +1744,37 @@ INSERT INTO `schedule_day` (`id`, `fk_doctor_id`, `fk_schedule_id`, `location_id
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `staff`
+--
+
+CREATE TABLE `staff` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `contact1` varchar(50) NOT NULL,
+  `contact2` varchar(50) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `address` varchar(1000) NOT NULL,
+  `recovery_contact` varchar(50) NOT NULL,
+  `recovery_email` varchar(100) NOT NULL,
+  `fk_location_id` int(11) NOT NULL,
+  `fk_doctor_id` int(11) NOT NULL,
+  `fk_user_id` int(11) NOT NULL,
+  `created_date` datetime NOT NULL,
+  `is_active` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `staff`
+--
+
+INSERT INTO `staff` (`id`, `first_name`, `last_name`, `contact1`, `contact2`, `email`, `address`, `recovery_contact`, `recovery_email`, `fk_location_id`, `fk_doctor_id`, `fk_user_id`, `created_date`, `is_active`) VALUES
+(5, 'Romney', 'mitty', '32423', '23423', 'us@me.com', 'old white house', '34123', 'reco@mars.com', 0, 18, 58, '2016-05-20 00:42:29', 1),
+(6, 'sdf', 'sdf', '34123', 'asdf', '42134', 'dfasdf', '324', 'dsd', 13, 18, 59, '2016-05-20 21:01:12', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `work_locations`
 --
 
@@ -1444,7 +1790,7 @@ CREATE TABLE `work_locations` (
 --
 
 INSERT INTO `work_locations` (`id`, `fk_doctor_id`, `name`, `description`) VALUES
-(12, 18, 'Panjim edited', ''),
+(12, 18, 'Panjim', ''),
 (13, 18, 'Margaon change', ''),
 (14, 18, 'asdf', ''),
 (15, 18, 'Temp', ''),
@@ -1458,6 +1804,12 @@ INSERT INTO `work_locations` (`id`, `fk_doctor_id`, `name`, `description`) VALUE
 -- Indexes for table `appointment`
 --
 ALTER TABLE `appointment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `delivery_methods`
+--
+ALTER TABLE `delivery_methods`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -1494,7 +1846,7 @@ ALTER TABLE `patient`
 -- Indexes for table `patient_birth_details`
 --
 ALTER TABLE `patient_birth_details`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`fk_patient_id`);
 
 --
 -- Indexes for table `patient_medication_programme`
@@ -1521,6 +1873,12 @@ ALTER TABLE `schedule_day`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `work_locations`
 --
 ALTER TABLE `work_locations`
@@ -1536,6 +1894,11 @@ ALTER TABLE `work_locations`
 ALTER TABLE `appointment`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT for table `delivery_methods`
+--
+ALTER TABLE `delivery_methods`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
 -- AUTO_INCREMENT for table `doctor`
 --
 ALTER TABLE `doctor`
@@ -1544,7 +1907,7 @@ ALTER TABLE `doctor`
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
 --
 -- AUTO_INCREMENT for table `medication_programme`
 --
@@ -1559,22 +1922,17 @@ ALTER TABLE `medication_programme_list`
 -- AUTO_INCREMENT for table `patient`
 --
 ALTER TABLE `patient`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
---
--- AUTO_INCREMENT for table `patient_birth_details`
---
-ALTER TABLE `patient_birth_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 --
 -- AUTO_INCREMENT for table `patient_medication_programme`
 --
 ALTER TABLE `patient_medication_programme`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `patient_medication_programme_list`
 --
 ALTER TABLE `patient_medication_programme_list`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 --
 -- AUTO_INCREMENT for table `schedule`
 --
@@ -1585,6 +1943,11 @@ ALTER TABLE `schedule`
 --
 ALTER TABLE `schedule_day`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=229053;
+--
+-- AUTO_INCREMENT for table `staff`
+--
+ALTER TABLE `staff`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `work_locations`
 --
