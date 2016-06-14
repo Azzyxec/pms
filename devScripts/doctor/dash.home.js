0ab0eb34ef7aeca09673bf67e7e995a6a99db3c3
nicolasXec <akhilshaikh28@gmail.com>
2016-06-14 20:11:27 +0530

    left calendar intialized

--- devScripts/doctor/dash.home.js
+++ devScripts/doctor/dash.home.js
@@ -82,7 +82,7 @@ $(document).ready(function(){
 
     var todayAppointmentListView = {
       init:function(){
-        this.dateInput = $('#appointment-list-date');
+        this.dateInput = $('#appointment-list-date1');
         this.locationSelect = $('#appointment-list-locations-sel');
         this.goButton = $('#appointment-list-go-btn');
 
@@ -109,6 +109,7 @@ $(document).ready(function(){
 
 
         this.dateInput.datetimepicker({
+          inline:true,
           format: 'DD-MM-YYYY'
         });
 
