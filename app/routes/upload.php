5303b889a65b7a3bcdc0ab79f38d77471f554f6d
nicolasXec <akhilshaikh28@gmail.com>
2016-07-04 21:54:40 +0530

    patient image retrieved from database

--- app/routes/upload.php
+++ app/routes/upload.php
@@ -7,11 +7,11 @@ $app->group('/Upload',function(){
     $this->get('/upload',function($request, $response)){
         try{
            $upload_dir = 'public/uploads/';
-$upload_handler = new UploadHandler(array(
+           $upload_handler = new UploadHandler(array(
                         'max_file_size' => 1048576, //1MB file size
                         'image_file_types' => '/\.(gif|jpe?g|png|mp4|mp3)$/i',
                         'upload_dir' => $upload_dir,
-                        'upload_url' => 'http://localhost/multi_file_upload/uploads/',
+                        'upload_url' => 'index.php/Upload/upload/',
                         'thumbnail' => array('max_width' => 80,'max_height' => 80)
                         ));
             $data = array('status' => "1", 'data' => $upload_handler, 'message' => "success" );
@@ -19,11 +19,11 @@ $upload_handler = new UploadHandler(array(
         } catch (Exception $e){
               $data = array('status' => "-1", 'data' => "", 'message' => "exception in controller " . $e->getMessage() );
       return $response->withJson($data);
-            
+
         }
     }
-    
-    
+
+
 })
 
-?> 
+?>
